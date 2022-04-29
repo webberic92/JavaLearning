942d9763ddbb26a7afc95bf7d289bb8e1524100c
eric.b.webb13.civ@mail.mil <eric.b.webb13.civ@mail.mil>
2022-04-28 22:16:18 -0400

    done for the night

--- codeWars/toCamelCase/Solution.java
+++ codeWars/toCamelCase/Solution.java
@@ -30,3 +30,16 @@ class Solution {
   }
 
 }
+
+
+// class Solution{
+
+//   static String toCamelCase(String s){
+//     Matcher m = Pattern.compile("[_|-](\\w)").matcher(s);
+//     StringBuffer sb = new StringBuffer();
+//     while (m.find()) {
+//         m.appendReplacement(sb, m.group(1).toUpperCase());
+//     }
+//     return m.appendTail(sb).toString();
+//   }
+// }
